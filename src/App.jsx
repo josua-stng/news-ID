09cc42012bcae61e00013f7ab8e0d1541f38fcb7
Josua <josuasitanggang51@gmail.com>
2023-01-25 10:19:56 +0700

    make routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './routes/Home'
+import Sports from './routes/Sports'
 
 function App() {
   return (
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/sports' element={<Sports/>}/>
       </Routes>
   )
 }
