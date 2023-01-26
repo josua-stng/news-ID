e55fed2edd84498318d3469de90a694da34dd6b9
Josua <josuasitanggang51@gmail.com>
2023-01-26 19:21:56 +0700

    add some components route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './routes/Home'
-import Sports from './routes/Sports'
+import { Route, Routes } from "react-router-dom";
+import Entertaiment from "./routes/Entertaiment";
+import Health from "./routes/Health";
+import Home from "./routes/Home";
+import Sports from "./routes/Sports";
+import Technology from "./routes/Technology";
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/sports' element={<Sports/>}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sports" element={<Sports />} />
+      <Route path="/entertaiment" element={<Entertaiment />} />
+      <Route path="/health" element={<Health />} />
+      <Route path="/technology" element={<Technology />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
